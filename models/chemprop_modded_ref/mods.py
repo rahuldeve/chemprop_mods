f5cfcb292f342873bbe5fb2ff7e1079f33954a62
Rahul Dev <rahul.e.dev@gmail.com>
2026-06-01 20:29:34 -0400

    Revert "update residual ffn to use post norm"
    This reverts commit 0f2081f03666e083a630c56d0f26046365fedae8.

--- models/chemprop_modded_ref/mods.py
+++ models/chemprop_modded_ref/mods.py
@@ -20,7 +20,7 @@ class ResidualFFN(torch.nn.Module):
         )
 
     def forward(self, inp, res):
-        return self.norm(self.non_linear(inp) + res)
+        return self.non_linear(self.norm(inp)) + res
 
 
 class ModdedBondMessagePassing(BondMessagePassing):
