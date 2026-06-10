4772c1bea435b5202bd718e368070ff8954c0c96
Rahul Dev <rahul.e.dev@gmail.com>
2026-06-10 12:29:09 -0400

    reduce early stopping patience

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class SplitType(StrEnum):
 class TrainConfig:
     batch_size: int = 64
     max_epochs: int = 50
-    early_stopping_patience: int = 20
+    early_stopping_patience: int = 10
     random_state: int = 42
